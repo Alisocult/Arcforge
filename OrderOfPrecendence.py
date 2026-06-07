9522885213c0b498558b295f10fe4576d6d0304d
Arcforge <gr0182443@gmail.com>
2026-06-06 22:31:18 -0300

    Add files via upload

---
+++ OrderOfPrecendence.py
@@ -0,0 +1,16 @@
+# (Operador) + adição
+# (Operador) - Subtração
+# (Operador) * Multiplicação
+# (Operador) / Divisão
+# (Operador) ** Potência
+# (Operador) // Divisão inteira
+# (Operador) % Resto da Divisão
+
+# 1 - ()
+# 2 - **
+# 3 - * | / | // | %
+# 4 - + | -
+
+print(5 + 3 * 2)
+print(3 * 5 + 4 ** 2)
+print(3 * (5 + 4) ** 2)
